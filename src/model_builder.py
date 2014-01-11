875dc30d9356ea5e785c051a18ae7f8ccb4c8bea
Wesley Werner <wez@darknet.co.za>
2014-01-11 07:55:59 +0200

    body builder methods to add and remove parts, calculate accuracy

--- src/model_builder.py
+++ src/model_builder.py
@@ -49,10 +49,10 @@ class Builder(object):
     def __init__(self, model):
         
         self.model = model
-        self.used_parts = []
+        self._used_parts = []
     
     def refresh_parts(self):
-        self.used_parts = list(DEFAULT_PARTS.get(self.model.level, []))
+        self._used_parts = list(DEFAULT_PARTS.get(self.model.level, []))
 
     def get_level_parts(self):
         """
@@ -65,3 +65,40 @@ class Builder(object):
             if k <= self.model.level:
                 combined.extend(v)
         return combined
+
+    def add_part(self, part):
+        """
+        Add a part to be used in buidling a body or item.
+        
+        """
+        
+        if self._used_parts.count(part) == 0:
+            self._used_parts.append(part)
+            print('added part %s' % (part,))
+    
+    def remove_part(self, part):
+        """
+        Remove a part from construction.
+        
+        """
+        
+        if part in self._used_parts:
+            self._used_parts.remove(part)
+            print('removed part %s' % (part,))
+
+    @property
+    def accuracy(self):
+        """
+        Gets a percentage of accuracy of the build body
+        as determined by the completion parts.
+        
+        """
+        
+        matches = 0.0
+        for part in self._used_parts:
+            if part in COMPLETION_PARTS[self.model.level]:
+                matches += 1.0
+        if matches == 0.0:
+            return 0
+        else:
+            return round(matches / len(COMPLETION_PARTS[self.model.level]) * 100)
