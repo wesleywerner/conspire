509b7691279cd37c202c9b30a5595a6e86de36a4
Wesley Werner <wez@darknet.co.za>
2014-01-11 18:36:05 +0200

    add results image

--- src/view.py
+++ src/view.py
@@ -308,6 +308,8 @@ class View(object):
             self.background = pygame.image.load(os.path.join('..', 'data', 'build-screen.png')).convert()
         if self.model.state == STATE_UFO:
             self.background = pygame.image.load(os.path.join('..', 'data', 'ufo-screen.png')).convert()
+        if self.model.state == STATE_RESULTS:
+            self.background = pygame.image.load(os.path.join('..', 'data', 'results-screen.png')).convert()
 
     def load_sprites(self):
         """
